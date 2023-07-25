9b8e2d7b23a697fe292228e3947e36479e3d3a2d
zfj <fujian.zfj@alibaba-inc.com>
2023-07-25 14:58:36 +0800

    remove gflag

--- java/crossbuild/build-win.bat
+++ java/crossbuild/build-win.bat
@@ -8,7 +8,7 @@ if exist librocksdbjni-win64.dll del librocksdbjni-win64.dll
 mkdir build && cd build
 
 
-cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=20 -DJNI=1 -DGFLAGS=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DZSTD=1 -DXPRESS=1 ..
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=20 -DJNI=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DZSTD=1 -DXPRESS=1 ..
 "%MSBUILD%" rocksdb.sln /p:Configuration=Release
 
 cd ..
