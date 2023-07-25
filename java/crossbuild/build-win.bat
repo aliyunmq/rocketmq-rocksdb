5da7c5083efa2a074a689b028d6b171ff7399c1a
zfj <fujian.zfj@alibaba-inc.com>
2023-07-25 10:17:43 +0800

    build

--- java/crossbuild/build-win.bat
+++ java/crossbuild/build-win.bat
@@ -1,14 +1,15 @@
 :: install git, java 8, maven, visual studio community 15 (2017)
 
 set MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
+set THIRDPARTY_HOME=D:\rocketmq-rocksdb
 
 if exist build rd /s /q build
 if exist librocksdbjni-win64.dll del librocksdbjni-win64.dll
 mkdir build && cd build
 
-cmake -G "Visual Studio 17 2022" -DWITH_JNI=1 -DWITH_SNAPPY=1 -DWITH_LZ4=1 -DWITH_ZLIB=1 -DWITH_ZSTD=1 ..
 
-"%MSBUILD%" rocksdb.sln /p:Configuration=Release /m
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=20 -DJNI=1 -DGFLAGS=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DZSTD=1 -DXPRESS=1 ..
+"%MSBUILD%" rocksdb.sln /p:Configuration=Release
 
 cd ..
 
